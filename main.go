6f232b4e2898ae3897eb4f1a9bc7eff637268b2d
zeelna <143965319+zeelna@users.noreply.github.com>
2026-07-04 12:08:48 +0300

    rescued maing.go from merge-conflict

--- main.go
+++ main.go
@@ -17,92 +17,52 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	// The idea is that instead of running the increment immediately,
-	// you delay it by wrapping it in a function that runs later (when a request actually arrives).
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("request received!") // runs on each request
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
+// Heplful Go doc links:
+// // https://pkg.go.dev/net/http#ServeMux.Handle
+// type Handler: https://pkg.go.dev/net/http#Handler
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	count := fmt.Sprintf("%v", cfg.fileserverHits.Load()) // convert int[32] into a string
-	countMsg := fmt.Sprintf("Hits: %v\n", count)          // prefix such that returns 'Hits: x'
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	if _, err := w.Write([]byte(countMsg)); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits = atomic.Int32{}
-	count := fmt.Sprintf("%v", cfg.fileserverHits.Load()) // convert int[32] into a string
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	if _, err := w.Write([]byte(count + "\n")); err != nil {
-		log.Fatal(err)
-	}
-}
-
-type requestHandlers struct {
-	allowed map[string]func(w http.ResponseWriter, req *http.Request)
-}
-
-func (rh *requestHandlers) register(name string, fn func(http.ResponseWriter, *http.Request)) {
-	if _, ok := (*rh).allowed[name]; ok {
-		fmt.Println("error, already exists")
-	}
-	(*rh).allowed[name] = fn
-}
+// https://pkg.go.dev/net/http#ResponseWriter
+//https://pkg.go.dev/net/http#ResponseWriter.Write
 
 func main() {
-	// Strategy pattern
-	handlers := requestHandlers{
-		allowed: make(map[string]func(w http.ResponseWriter, req *http.Request)),
-	}
-	handlers.register("/metrics", handlerMetrics)
-	handlers.register("/healthz", handlerHealth)
-	handlers.register("/reset", handlerReset)
-
-	apiCfg := apiConfig{
+	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
-	// When you register "/" with http.FileServer(http.Dir(".")), it serves all files under the current directory — including assets/logo.png.
-	// The file server path should be /app/ (with trailing slash), not /app
 	// --------------------------------------------------------
+
+	// We have many handlers, we don't want potential conflicts with the fileserver handler.
+	// Updated the fileserver to use the /app/ path instead of /.
+	// Not only will you need to mux.Handle the /app/ path,
+	// you'll also need to strip the /app prefix from the request path before passing it to the fileserver handler.
+	// You can do this using the http.StripPrefix function.
 	serverMux := http.NewServeMux()
-	// GET /
-	serverMux.Handle("/app/",
-		apiCfg.middlewareMetricsInc(
-			http.StripPrefix("/app", http.FileServer(http.Dir(current_directory)))))
+	// GET /app redirects to /app/ (to avoid GET /app vs GET /app/
+	serverMux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/app/", http.StatusMovedPermanently)
+	})
+	// GET /app/ serves files
+	// Motivation: now we remove /app from /app/, resulting in '/'. This is required. We cannot simply use serverMux.handle("/", ...)
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(current_directory))),
+	),
+	)
+	// --------------------------------------------------------
 	// Now that we've added a new handler (for path '/healthz' , we don't want potential conflicts with the fileserver handler.)
 	// Updated the fileserver to use the /app/ path instead of /. And for that, we used http.StripPrefix inside of serverMux.Handle() function call
 	// --------------------------------------------------------
 
-	// GET /healthz
-	// create 'readiness endpoint' for Chirpy server.
-	// Readiness endpoints are commonly used by external systems to check if our server is ready to receive traffic.
-	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-		w.WriteHeader(http.StatusOK)
-		if _, err := w.Write([]byte("OK\n")); err != nil {
-			log.Fatal(err)
-		}
-		// The endpoint should simply return a 200 OK status code indicating that it has started up successfully and is listening for traffic
-	}) // Later this endpoint can be enhanced to return a 503 Service Unavailable status code if the server is not ready.
+	// GET /healthz -- a create 'readiness endpoint' for Chirpy server.
+	// Motivation: Readiness endpoints are commonly used by external systems to check if our server is ready to receive traffic.
+	serverMux.HandleFunc("GET /api/healthz", apiCfg.handlerHealth) // Later this endpoint can be enhanced to return a 503 Service Unavailable status code if the server is not ready.
+	// --------------------------------------------------------
+	// GET /metrics -- how many people are viewing the site (until server is turned off)
+	// motivation: // how many requests are being made to serve our homepage - in essence, they want to know
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics) // return the count as plain text in the response body.
 	// --------------------------------------------------------
 
-	// GET /metrics
-	// motivation: // how many requests are being made to serve our homepage - in essence, they want to know how many people are viewing the site!
-	serverMux.HandleFunc("/metrics", apiCfg.handlerMetrics) // return the count as plain text in the response body.
-	// --------------------------------------------------------
-
-	// GET /metrics
-	serverMux.HandleFunc("/reset", apiCfg.handlerReset)
+	// GET /metrics -- reset to '0' many people are viewing the site!
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	// --------------------------------------------------------
 
 	server := http.Server{
@@ -113,6 +73,74 @@ func main() {
 		log.Fatal(err)
 	}
 	return
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// The idea is that instead of running the increment immediately,
+	// you delay it by wrapping it in a function that runs later (when a request actually arrives).
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Println("request received!") // runs on each request
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r) //
+	})
+	// do not forge the pattern here - middleware wraps an anonymous-function with different function signature
+}
+
+func (cfg *apiConfig) handlerHealth(w http.ResponseWriter, req *http.Request) {
+	// Header:
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache")
+
+	// Status Code: Send HTTP 200/ok
+	w.WriteHeader(http.StatusOK)
+
+	// Send 'OK' in response body
+	if _, err := w.Write([]byte("OK\n")); err != nil {
+		log.Fatal(err)
+	}
+	// The endpoint should simply return a 200 OK status code indicating that it has started up successfully and is listening for traffic
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	// Header:
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache")
+
+	// Status Code: Send HTTP 200/ok
+	w.WriteHeader(http.StatusOK)
+
+	// Respone body
+	hits := cfg.fileserverHits.Load()
+	body := fmt.Sprintf(
+		`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, hits)
+
+	// Send count 'cfg.fileserverHits.Load()' in response body. Be sure to convert into []byte slice.
+	if _, err := w.Write([]byte(body)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	// reset the count of visits to the server
+	cfg.fileserverHits.Store(0)
+
+	// Header:
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache")
+
+	// Status Code: Send HTTP 200/ok
+	w.WriteHeader(http.StatusOK)
+
+	// Body: Send 'OK'
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // you can compile a binary and run server (in the background):
